6b53a4fc10fda1b0626228c65736adcefe3bca66
Alirea <2981883527@qq.com>
2025-06-23 22:08:24 +0800

    移动组件位置，提高可读性

---
+++ src/components/course.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import Classify from "@/components/Subtool/classify.vue";
+import Cardlist from "@/components/Subtool/cardlist.vue";
+</script>
+
+<template>
+<classify></classify>
+<cardlist></cardlist>
+</template>
+
+<style scoped lang="scss">
+
+</style>
