8a7ae72d924bc8ec93ed3fa1fc9af19c2ed930bd
Alirea <2981883527@qq.com>
2025-06-24 11:00:45 +0800

    home：引入top hot两个组件

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,14 @@
 <script setup>
 
-import Carousel from "@/components/Subtool/home/carousel.vue";
+import Carousel from "@/components/Subtool/home/carousel/carousel.vue";
+import Hot from "@/components/Subtool/home/hot.vue";
+import Top from "@/components/Subtool/home/top.vue";
 </script>
 
 <template>
   <carousel></carousel>
+  <hot></hot>
+  <top></top>
 </template>
 
 <style scoped lang="scss">
