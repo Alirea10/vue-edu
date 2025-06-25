7c07ec54f95a640e48c26d5cdfdb76328b312e59
Alirea <2981883527@qq.com>
2025-06-25 16:38:30 +0800

    news：增加主页新闻栏

---
+++ src/components/Subtool/home/news.vue
@@ -0,0 +1,88 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <p class="title">新闻动态</p>
+    <div class="news">
+      <div class="news__left">
+        <div class="news__left__item" v-for="i in 4" :key="i">
+          <div class="news__left__item__title">
+            <p>新闻标题 {{ i }}</p>
+          </div>
+          <div class="news__left__item__info">
+            <span>近日，教育部发布《教育部现行有效规章目录》的公告，教育部整理了47项规章的原文
+链接，点击规章名称即可查看。一起来看具体目录吧↓√√
+</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="news__right">
+        <n-carousel autoplay show-arrow>
+          <img
+              src="/assets/images/img.jpg"
+              alt="新闻图片">
+          <img
+              src="/assets/images/img.jpg"
+              alt="新闻图片">
+          <img
+              src="/assets/images/img.jpg"
+              alt="新闻图片">
+          <img
+              src="/assets/images/img.jpg"
+              alt="新闻图片">
+        </n-carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 80%;
+  margin: 10px auto 300px;
+}
+
+.title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 20px;
+
+}
+
+.news {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  height: 400px; // 可根据需要调整高度
+
+
+  &__left {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+
+    &__item {
+      height: 25%;
+      background-color: #f5f5f5;
+      &:hover{
+        background-color: #ffffff;
+      }
+    }
+  }
+
+  &__right {
+    width: 50%;
+    height: 100%;
+    display: flex;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+
+</style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -3,12 +3,14 @@
 import Carousel from "@/components/Subtool/home/carousel/carousel.vue";
 import Hot from "@/components/Subtool/home/hot.vue";
 import Top from "@/components/Subtool/home/top.vue";
+import News from "@/components/Subtool/home/news.vue";
 </script>
 
 <template>
   <carousel></carousel>
   <hot></hot>
   <top></top>
+  <news></news>
 </template>
 
 <style scoped lang="scss">
